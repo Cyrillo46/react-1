b2d1978fb187ee1878742e1b3363d5538b667fb4
Cyrillo46 <yahooisbetter46@gmail.com>
2023-05-27 13:08:10 -0400

    Starting Best Seller practice project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Greeting from './App';
+
+
+ const BookList = () => {
+  return (
+    <section>BookList</section>
+  )
+}
+
+ const Book = () => {
+  return (
+    <article>Book</article>
+  )
+}
+
+const Image = () => <h2>Image placeholder</h2>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting />);
+root.render(<BookList />);
 
