7d08264ea2bd4389897ad5331b3c5de945fea8bf
Cyrillo46 <yahooisbetter46@gmail.com>
2023-05-28 22:17:14 -0400

    preparing to render two books

--- src/index.js
+++ src/index.js
@@ -4,15 +4,23 @@ import './index.css';
 // import author from 'Author.js'
 // import title from 'Title.js'
 // import img from 'Image.js'
-const author = 'Raymond K Boseman'
-const title = "Things Black People Weren't Taught"
-const img = "https://m.media-amazon.com/images/I/31DRXvr5JKL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
+const FirstBook =  {
+author: 'Raymond K Boseman',
+title: "Things Black People Weren't Taught",
+img: "https://m.media-amazon.com/images/I/31DRXvr5JKL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
+}
+
+const SecondBook =  {
+  author: 'Chuck Callaway',
+  title: "Practical Joint Manipulation",
+  img: "https://m.media-amazon.com/images/I/41GP3IUNOLL.jpg"
+  }
 
  const BookList = () => {
   return (
     <section className="booklist">
-      <Book title={title} author={author} img={img} />
-      <Book title={title} author={author} img={img} />
+      <Book title={FirstBook.title} author={FirstBook.author} img={FirstBook.img} />
+      <Book title={SecondBook.title} author={SecondBook.author} img={SecondBook.img} />
       </section>
   )
 }
@@ -20,9 +28,9 @@ const img = "https://m.media-amazon.com/images/I/31DRXvr5JKL._SY291_BO1,204,203,
  const Book = (props) => {
    return (
       <article className="book">
-    <img src={img} alt={title} />
-    <h2>{title}</h2>
-    <h4>{author}</h4>
+    <img src={SecondBook.img} alt={SecondBook.title} />
+    <h2>{SecondBook.title}</h2>
+    <h4>{SecondBook.author}</h4>
     <p>{props.title}</p>
     <p>{props.job}</p>
     <p>{props.number}</p>
