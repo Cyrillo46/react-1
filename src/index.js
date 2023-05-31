fe8f65b436f10a94f4e9cd6d4bd2272b8ed0abfa
Cyrillo46 <yahooisbetter46@gmail.com>
2023-05-31 00:26:25 -0400

    cleaned index.js file

--- src/index.js
+++ src/index.js
@@ -15,18 +15,6 @@ const Books = [
       img: "https://m.media-amazon.com/images/I/41GP3IUNOLL.jpg"
       }
 ]
-/*
-const Book = (props) => {
-  const { title, author, img} = props;
-      return (
-      <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      </article>
-  )
-}
-*/
 
  const BookList = () => {
    return (
